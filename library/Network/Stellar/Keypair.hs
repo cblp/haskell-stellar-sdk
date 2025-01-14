900e898b2bdb090b045c032f10573f5d4aa45fd2
Yuriy Syrovetskiy <git-orbi@cblp.su>
2025-01-14 13:46:48 +0100

    Remove DRBG dependency; Update resolver to LTS-21

--- library/Network/Stellar/Keypair.hs
+++ library/Network/Stellar/Keypair.hs
@@ -17,13 +17,15 @@ module Network.Stellar.Keypair
     )where
 
 import           Control.Monad (guard)
-import           Crypto.Random.DRBG
+import           Crypto.Random (getSystemDRG, randomBytesGenerate)
 import           Crypto.Sign.Ed25519
 import           Data.Bits
-import qualified Data.ByteString as B
+import           Data.ByteString (ByteString)
+import qualified Data.ByteString as BS
 import           Data.ByteString.Base32 (decodeBase32, encodeBase32)
 import           Data.Maybe (fromJust, fromMaybe)
-import qualified Data.Text as T
+import           Data.Text (Text)
+import qualified Data.Text as Text
 import           Data.Text.Encoding (encodeUtf8)
 import           Data.Word (Word16, Word8)
 import           GHC.Stack (HasCallStack)
@@ -31,74 +33,74 @@ import           GHC.Stack (HasCallStack)
 data KeyPair = KeyPair
     { kpPublicKey   :: PublicKey
     , kpPrivateKey  :: SecretKey
-    , kpSeed        :: B.ByteString
+    , kpSeed        :: ByteString
     }
 
 instance Show KeyPair where
     show (KeyPair public _ seed) =
-        "KeyPair {" ++ T.unpack (encodePublic $ unPublicKey public) ++ ", "
-        ++ T.unpack (encodePrivate seed) ++ "}"
+        "KeyPair {" ++ Text.unpack (encodePublic $ unPublicKey public) ++ ", "
+        ++ Text.unpack (encodePrivate seed) ++ "}"
 
 generateKeypair :: IO KeyPair
 generateKeypair = do
-    gen <- newGenIO :: IO CtrDRBG
-    Right (randomBytes, _) <- pure $ genBytes 32 gen
+    gen <- getSystemDRG
+    let (randomBytes, _) = randomBytesGenerate 32 gen
     return $ fromSeed randomBytes
 
-fromSeed :: B.ByteString -> KeyPair
+fromSeed :: ByteString -> KeyPair
 fromSeed seed = KeyPair public private seed
     where (public, private) = fromJust $ createKeypairFromSeed_ seed
 
-fromPrivateKey :: T.Text -> Maybe KeyPair
+fromPrivateKey :: Text -> Maybe KeyPair
 fromPrivateKey = fmap fromSeed . decodePrivate
 
-fromPrivateKey' :: HasCallStack => T.Text -> KeyPair
+fromPrivateKey' :: HasCallStack => Text -> KeyPair
 fromPrivateKey' = fromSeed . decodePrivate'
 
-signatureHint :: KeyPair -> B.ByteString
-signatureHint = B.drop 28 . unPublicKey . kpPublicKey
+signatureHint :: KeyPair -> ByteString
+signatureHint = BS.drop 28 . unPublicKey . kpPublicKey
 
 
-encodePublic :: B.ByteString -> T.Text
+encodePublic :: ByteString -> Text
 encodePublic = encodeKey EncodingAccount
 
-encodePublicKey :: PublicKey -> T.Text
+encodePublicKey :: PublicKey -> Text
 encodePublicKey = encodePublic . unPublicKey
 
-encodePrivate :: B.ByteString -> T.Text
+encodePrivate :: ByteString -> Text
 encodePrivate = encodeKey EncodingSeed
 
-decodePublic :: T.Text -> Maybe B.ByteString
+decodePublic :: Text -> Maybe ByteString
 decodePublic = decodeKey EncodingAccount
 
-decodePublicKey :: T.Text -> Maybe PublicKey
+decodePublicKey :: Text -> Maybe PublicKey
 decodePublicKey = fmap PublicKey . decodeKey EncodingAccount
 
-decodePublic' :: T.Text -> B.ByteString
+decodePublic' :: Text -> ByteString
 decodePublic' = decodeKey' EncodingAccount
 
-decodePublicKey' :: T.Text -> PublicKey
+decodePublicKey' :: Text -> PublicKey
 decodePublicKey' = PublicKey . decodePublic'
 
-decodePrivate :: T.Text -> Maybe B.ByteString
+decodePrivate :: Text -> Maybe ByteString
 decodePrivate = decodeKey EncodingSeed
 
-decodePrivate' :: HasCallStack => T.Text -> B.ByteString
+decodePrivate' :: HasCallStack => Text -> ByteString
 decodePrivate' = decodeKey' EncodingSeed
 
-decodeKey :: EncodingVersion -> T.Text -> Maybe B.ByteString
+decodeKey :: EncodingVersion -> Text -> Maybe ByteString
 decodeKey version key = do
     keyBlob <- either (const Nothing) Just $ decodeBase32 $ encodeUtf8 key
-    let (payload, checksum) = B.splitAt (B.length keyBlob - 2) keyBlob
-    (versionByte, keyData) <- B.uncons payload
+    let (payload, checksum) = BS.splitAt (BS.length keyBlob - 2) keyBlob
+    (versionByte, keyData) <- BS.uncons payload
     let versionCheck = versionByte == versionByteName version
         checksumCheck = crc16XmodemLE payload == checksum
     guard (versionCheck && checksumCheck)
     pure keyData
 
-decodeKey' :: HasCallStack => EncodingVersion -> T.Text -> B.ByteString
+decodeKey' :: HasCallStack => EncodingVersion -> Text -> ByteString
 decodeKey' version key =
-    fromMaybe (error $ "Decoding key failed " ++ T.unpack key) $
+    fromMaybe (error $ "Decoding key failed " ++ Text.unpack key) $
     decodeKey version key
 
 data EncodingVersion = EncodingAccount | EncodingSeed | EncodingPreAuthTx | EncodingSha256Hash
@@ -109,16 +111,16 @@ versionByteName EncodingSeed       = 144
 versionByteName EncodingPreAuthTx  = 152
 versionByteName EncodingSha256Hash = 184
 
-encodeKey :: EncodingVersion -> B.ByteString -> T.Text
-encodeKey version key = encodeBase32 $ payload `B.append` checksum
+encodeKey :: EncodingVersion -> ByteString -> Text
+encodeKey version key = encodeBase32 $ payload <> checksum
     where
         versionByte = versionByteName version
-        payload = versionByte `B.cons` key
+        payload = versionByte `BS.cons` key
         checksum = crc16XmodemLE payload
 
-crc16XmodemLE :: B.ByteString -> B.ByteString
-crc16XmodemLE bs = B.pack [fromIntegral $ checksum .&. 0xFF, fromIntegral $ checksum `shiftR` 8]
-    where checksum = B.foldl crcRound 0 bs
+crc16XmodemLE :: ByteString -> ByteString
+crc16XmodemLE bs = BS.pack [fromIntegral $ checksum .&. 0xFF, fromIntegral $ checksum `shiftR` 8]
+    where checksum = BS.foldl crcRound 0 bs
 
 crcRound :: Word16 -> Word8 -> Word16
 crcRound crc byte = crc2
